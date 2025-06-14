3d43dcc3faf3119c709c15a5e2dfafea0bea9d5d
DDEE24 <thavatchai.personal@gmail.com>
2025-06-14 13:16:41 +0700

    delete print
    olljgouhfvuy

--- expense.py
+++ expense.py
@@ -4,5 +4,3 @@ class Expense:
         self.name = name
         self.category = category
         self.amount = amount
-    def __intro__(self):
-        print(f"{self.name}.{self.category}.{self.amount}")
