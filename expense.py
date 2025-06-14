bb76bc2242ea35ff1b8f29ad7f7408108643d250
DDEE24 <thavatchai.personal@gmail.com>
2025-06-14 13:15:31 +0700

    print
    klbgyujgy6r53w

--- expense.py
+++ expense.py
@@ -4,3 +4,5 @@ class Expense:
         self.name = name
         self.category = category
         self.amount = amount
+    def __intro__(self):
+        print(f"{self.name}.{self.category}.{self.amount}")
